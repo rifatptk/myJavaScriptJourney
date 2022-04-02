985d881eec6782dd723c0e3cc0a41af813a17b9b
RIfat Shikder <rifatptk@gmail.com>
2022-04-02 12:23:09 +0600

    added while loop

--- JS for loop/JS for loop.js
+++ JS for loop/JS for loop.js
@@ -2,6 +2,7 @@
 // for(init; condition; increament/ decreament){
 //     codes to be executed
 // }
+//both init and condition section of an for loop is optional.
 
 // example:
 for(i = 0; i <= 10; i++){
---
+++ JS while loop/JS while loop.js
@@ -0,0 +1,28 @@
+//The while loop loops through a block of code as long as a specified condition is true.
+
+//Syntax:
+// while(condition){
+//     block of code to be executed
+// }
+
+//Example: while
+
+let i = 0;
+let text = ""
+while(i <= 10){
+    text += "The number is: " + i + "\n";
+    i++;
+}
+console.log(text);
+
+
+//Example: do while
+
+let i2 = 11;
+let text2 = ""
+do{
+    text2 += "The number is: " + i2 +"\n";
+    i2++;
+}while(i2 <= 20);
+
+console.log(text2);
